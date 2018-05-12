292c64a3910240c46acfbd5cfa631bfbfd76755a
filipe56 <filipe56@gmail.com>
2018-05-12 13:27:29 -0300

    Initial

---
+++ ArchiveService/ArchiveService/Controllers/DocumentoController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ArchiveService.Models;
+
+namespace ArchiveService.Controllers
+{
+    public class DocumentoController : ApiController
+    {
+        private DBArquivoEntities db = new DBArquivoEntities();
+
+        // GET: api/Documento
+        public IQueryable<tblDocumento> GettblDocumento()
+        {
+            return db.tblDocumento;
+        }
+
+        // GET: api/Documento/5
+        [ResponseType(typeof(tblDocumento))]
+        public IHttpActionResult GettblDocumento(int id)
+        {
+            tblDocumento tblDocumento = db.tblDocumento.Find(id);
+            if (tblDocumento == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tblDocumento);
+        }
+
+        // PUT: api/Documento/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PuttblDocumento(int id, tblDocumento tblDocumento)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tblDocumento.id_Documento)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tblDocumento).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!tblDocumentoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Documento
+        [ResponseType(typeof(tblDocumento))]
+        public IHttpActionResult PosttblDocumento(tblDocumento tblDocumento)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.tblDocumento.Add(tblDocumento);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (tblDocumentoExists(tblDocumento.id_Documento))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = tblDocumento.id_Documento }, tblDocumento);
+        }
+
+        // DELETE: api/Documento/5
+        [ResponseType(typeof(tblDocumento))]
+        public IHttpActionResult DeletetblDocumento(int id)
+        {
+            tblDocumento tblDocumento = db.tblDocumento.Find(id);
+            if (tblDocumento == null)
+            {
+                return NotFound();
+            }
+
+            db.tblDocumento.Remove(tblDocumento);
+            db.SaveChanges();
+
+            return Ok(tblDocumento);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool tblDocumentoExists(int id)
+        {
+            return db.tblDocumento.Count(e => e.id_Documento == id) > 0;
+        }
+    }
+}
---
+++ ArchiveService/ArchiveService/Controllers/UsuarioController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ArchiveService.Models;
+
+namespace ArchiveService.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        private DBArquivoEntities db = new DBArquivoEntities();
+
+        // GET: api/Usuario
+        public IQueryable<tblUsuario> GettblUsuario()
+        {
+            return db.tblUsuario;
+        }
+
+        // GET: api/Usuario/5
+        [ResponseType(typeof(tblUsuario))]
+        public IHttpActionResult GettblUsuario(int id)
+        {
+            tblUsuario tblUsuario = db.tblUsuario.Find(id);
+            if (tblUsuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tblUsuario);
+        }
+
+        // PUT: api/Usuario/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PuttblUsuario([FromBody]int id, [FromBody] tblUsuario tblUsuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tblUsuario.id_Usuario)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tblUsuario).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!tblUsuarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Usuario
+        //[ResponseType(typeof(tblUsuario))]
+        public IHttpActionResult PosttblUsuario([FromBody]tblUsuario tblUsuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.tblUsuario.Add(tblUsuario);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = tblUsuario.id_Usuario }, tblUsuario);
+        }
+
+        // DELETE: api/Usuario/5
+        [ResponseType(typeof(tblUsuario))]
+        public IHttpActionResult DeletetblUsuario(int id)
+        {
+            tblUsuario tblUsuario = db.tblUsuario.Find(id);
+            if (tblUsuario == null)
+            {
+                return NotFound();
+            }
+
+            db.tblUsuario.Remove(tblUsuario);
+            db.SaveChanges();
+
+            return Ok(tblUsuario);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool tblUsuarioExists(int id)
+        {
+            return db.tblUsuario.Count(e => e.id_Usuario == id) > 0;
+        }
+    }
+}
